9132f2175cea34ac57456153da021bb213551b3e
Josiah Parry <josiah.parry@gmail.com>
2025-01-03 19:23:09 -0800

    add renv.lock parsing

--- rt-lib/src/lib.rs
+++ rt-lib/src/lib.rs
@@ -2,3 +2,5 @@ pub mod discover;
 pub use discover::{RVersion, RVersions};
 // we will discover them based on OS
 // https://doc.rust-lang.org/reference/conditional-compilation.html#target_os
+
+pub mod renv;
---
+++ rt-lib/src/renv.rs
@@ -0,0 +1,92 @@
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct RenvLock {
+    pub r: RenvRVersion,
+    // Note that each apckage should be untagged
+    pub packages: HashMap<String, RenvPackage>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct RenvRVersion {
+    pub version: String,
+    pub repositories: Vec<RenvRepository>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RenvRepository {
+    #[serde(rename = "Name")]
+    pub name: Option<String>,
+    #[serde(rename = "URL")]
+    pub url: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct RenvPackage {
+    pub package: String,
+    pub version: String,
+    pub source: String,
+    pub repository: String,
+    pub requirements: Option<Vec<String>>,
+    pub hash: String,
+}
+
+impl RenvLock {
+    pub fn read<P: AsRef<Path>>(path: P) -> anyhow::Result<Self> {
+        let file = File::open(path.as_ref())?;
+        let reader = BufReader::new(file);
+        let renv_lock: RenvLock = serde_json::from_reader(reader)?;
+        Ok(renv_lock)
+    }
+
+    pub fn write<P: AsRef<Path>>(&self, path: P) -> anyhow::Result<()> {
+        let f = std::fs::File::create(&path)?;
+        serde_json::to_writer_pretty(&f, self)?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_renv_lock() {
+        let path = "../tests/renv.lock";
+        match RenvLock::read(path) {
+            Ok(renv_lock) => {
+                println!("{:#?}", renv_lock);
+            }
+            Err(err) => {
+                panic!("Failed to parse renv.lock: {}", err);
+            }
+        }
+    }
+
+    #[test]
+    fn round_trip_renv() -> anyhow::Result<()> {
+        let lock = RenvLock::read("../tests/renv.lock")?;
+        // Write the parsed RenvLock to a temporary file
+        let temp_file_path = std::env::temp_dir().join("temp_renv.lock");
+
+        lock.write(&temp_file_path)?;
+        // Read the temporary file back and deserialize it
+        let read_lock = RenvLock::read(&temp_file_path)?;
+
+        // Print out the deserialized RenvLock
+        println!("{:#?}", read_lock);
+
+        // Clean up the temporary file
+        std::fs::remove_file(&temp_file_path)?;
+
+        Ok(())
+    }
+}
